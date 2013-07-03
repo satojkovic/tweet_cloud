9ee10480dd1c1190fcd1330f81f4ef1db1b1b523
satojkovic <satojkovic@gmail.com>
2013-07-03 22:59:17 +0900

    use docopt

--- tweet_cloud.py
+++ tweet_cloud.py
@@ -1,18 +1,32 @@
-#-*- conding: utf-8 -*-
+#-*- coding: utf-8 -*-
+
+"""Usage: tweet_cloud.py [-f FILE]
+
+-f FILE    specify input file [default: tweets.csv]
+"""
 
 from pytagcloud import create_tag_image, make_tags
 from pytagcloud.lang.counter import get_tag_counts
 
+import MeCab
+import csv
+from docopt import docopt
+import re
+
 def main():
-    TEXT = "A tag cloud is a visual representation for text data, typically\
-used to depict keyword metadata on websites, or to visualize free form text."
+    args = docopt(__doc__, version="1.0")
 
-    tags = make_tags(get_tag_counts(TEXT), maxsize=80)
+    f = args['-f']
+    cr = csv.reader(open(f, 'r'), delimiter=',')
+    for row in cr:
+        tweet = row[-1]
+        tweet_date = "YYYY-MM-DD HH:MM:SS"
+        for r in row:
+            s = re.search('((\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+))', r)
+            if s is not None:
+                tweet_date = s.group(1)
 
-    create_tag_image(tags, 'cloud.png', size=(640, 480), fontname='Lobster')
-
-    import webbrowser
-    webbrowser.open('cloud.png')
+        print tweet_date, tweet
 
 if __name__ == '__main__':
     main()
